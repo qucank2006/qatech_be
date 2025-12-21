10958a9a94302c5d2a0d13e01dcb080c00471450
Tran Le Quoc Anh <anhtlq.24itb@vku.udn.vn>
2025-12-21 19:12:30 +0700

    be

--- src/services/authService.js
+++ src/services/authService.js
@@ -86,17 +86,34 @@ exports.createOTP = async (email) => {
   });
 
   // 5) Gửi email
+  const htmlContent = `
+    <div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
+      <div style="margin:50px auto;width:70%;padding:20px 0">
+        <div style="border-bottom:1px solid #eee">
+          <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">QATech</a>
+        </div>
+        <p style="font-size:1.1em">Xin chào,</p>
+        <p>Sử dụng mã OTP sau để hoàn tất thủ tục khôi phục mật khẩu của bạn. Mã có hiệu lực trong 5 phút.</p>
+        <h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">${otp}</h2>
+        <p style="font-size:0.9em;">Xin cảm ơn,<br />QATech Team</p>
+        <hr style="border:none;border-top:1px solid #eee" />
+        <div style="float:right;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300">
+          <p>QATech Inc</p>
+        </div>
+      </div>
+    </div>
+  `;
+
   await transporter.sendMail({
     from: process.env.EMAIL_USER,
     to: email,
     subject: "Mã khôi phục mật khẩu QATech",
-    text: `Mã OTP của bạn là ${otp}. Mã có hiệu lực trong 5 phút.`,
+    html: htmlContent,
   });
 
   return {
     status: 200,
     msg: "OTP đã được gửi qua email",
-    otp // giữ lại để test FE, sau này xóa
   };
 };
 // verify OTP
